1a00ff562ef1792969164a865e47f10f5b48343b
littlejeem <jlivin25@live.co.uk>
2019-06-22 23:41:56 +0100

    formalised structure more, added options

--- config.sh
+++ config.sh
@@ -1,13 +1,31 @@
-#################
-## KODI CONFIG ##
-#################
+###################
+## "Kodi Config" ##
+###################
 #
 #
-kodiuser=
-kodipass=
-PORT=
-KODIADDRESS=
+kodiuser="" #username ofthe kodi interface on the local machin
+kodipass="" #password of same
+PORT="" #port that kodi is running under, usually 8080
 #
 #
-#################
-
+########################
+## "Machine Settings" ##
+########################
+#
+#
+LOCAL_IP="" #IP address of the local system
+REMOTE_IP="" #IP address of the remote system
+LOCAL_USER="" #username for the rsync local (pull too)
+REMOTE_USER="" #username for the rsync remote system (pull from)
+#
+#
+####################
+## "Sync Choices" ##
+####################
+#
+#
+FLACMUSIC="0" #1 to include, 0 to ignore. 0 is set as default
+ALACMUSIC="0" #1 to include, 0 to ignore. 0 is set as default
+TV="0" #1 to include, 0 to ignore. 0 is set as default
+MOVIES="0" #1 to include, 0 to ignore. 0 is set as default
+NFL="0" #1 to include, 0 to ignore. 0 is set as default
