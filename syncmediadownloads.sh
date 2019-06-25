588e7e19423ae67f069fb6b1f9656ddd4c04da91
littlejeem <jlivin25@live.co.uk>
2019-06-25 22:32:27 +0100

    stripped out swathes of irrelavent commenting, more ordered approach

--- syncmediadownloads.sh
+++ syncmediadownloads.sh
@@ -1,61 +1,32 @@
 #!/usr/bin/env bash
 #
+#############################################################################################
+## this script is to move downloaded media from a remote source, such as raspberry pi,     ##
+## it moves media from the remote to the media source (local machine) using an rsync pull  ##
+## script is in user bin folder but run from sudo or su cron not user                      ##
+#############################################################################################
 #
-####################################################################################################
-## this script is to move downloaded media from raspberry pi
-## it moves media from the pi to media pc using an rsync pull
-## script is in user bin folder but run from sudo or su cron not user
-##
-##
-## transmission downloads movies to:
-## /media/pi/USBSTORAGE/downloads/completed/transmission/movies
-## and tv shows to:
-## /media/pi/USBSTORAGE/downloads/completed/transmission/tv
-##
-##
-## then nzbToMedia script TorrentToMedia processes the files on completion and moves...
-## ...movies from:
-## /media/pi/USBSTORAGE/downloads/completed/transmission/movies
-## to:
-## /media/pi/USBSTORAGE/downloads/completed/movies
-##
-##
-##  ...TV from:
-## /media/pi/USBSTORAGE/downloads/completed/trasmission/tv
-## to:
-## /media/pi/USBSTORAGE/downloads/completed/tv
-##
-##
-## CouchPotato renames and moves the files from:
-## /media/pi/USBSTORAGE/downloads/completed/movies
-## to:
-## /media/pi/USBSTORAGE/Movies
-##
-##
-## SickRage renames and moves the files from:
-## /media/pi/USBSTORAGE/downloads/completed/tv
-## to:
-## /media/pi/USBSTORAGE/TV
-####################################################################################################
 #
 ###################################
 # Import sensitive data from file #
 ###################################
-#
 source .config.sh
 #
 #
 ####################
 ## set variables  ##
 ####################
-#
-locknamelong=`basename "$0"`    			# imports the name of this script
-lockname=${locknamelong::-3}    			# reduces the name to remove .sh
-logfolder=/home/jlivin25/bin/myscripts/scriptlogs/      # Where the logs are kept
-logname=$lockname.log  					# Uses the script name to create the log
-umaskset=0000						# Umask for 777 perms is 0000
+locknamelong=`basename "$0"` # imports the name of this script
+lockname=${locknamelong::-3} # reduces the name to remove .sh
+logfolder=/home/jlivin25/bin/myscripts/scriptlogs/ # Where the logs are kept
+logname=$lockname.log # Uses the script name to create the log
+umaskset=0000 # Umask for 777 perms is 0000
 kodilocal=http://$kodiuser:$kodipass@192.168.0.2:8080/jsonrpc
-#KODI 17/18 - Krypton Functions for library actions
+#
+#
+##############################################################
+## set FUNCTIONS - tested on KODI 17/18 for library actions ##
+##############################################################
 update_videolibrary () {
 curl --data-binary '{ "jsonrpc": "2.0", "method": "VideoLibrary.Scan", "id": "mybash"}' -H 'content-type: application/json;' $kodilocal
 }
@@ -74,81 +45,53 @@ curl --data-binary '{ "jsonrpc": "2.0", "method": "AudioLibrary.Clean", "id": "m
 #
 echo "----------------------------------------------------" > $logfolder/$logname
 echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - $locknamelong Started, sleeping for 3mins to allow network to start" >> $logfolder/$logname
-# add sleep for cron @reboot to allow tine for network to start
-sleep 1m
+sleep 1m #sleep for cron @reboot to allow tine for network to start
 #
 #
-#
-################################################################################################################################################################
-# start MUSIC sync
+######################
+## start MUSIC sync ##
+######################
 echo "----------------------------------------------------" >> $logfolder/$logname
 echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - Music sync started" >> $logfolder/$logname
 # sync flac source files first
 umask $umaskset
 rsync "$rsync_variable1" "$rsync_variable2" "$rsync_switch" "$REMOTE_USER"@"$REMOTE_IP":"$lossless_source" "$lossless_dest" >> $logfolder/$logname
-sleep 1
 /home/jlivin25/bin/myscripts/MusicSync.sh
-#update music library on Kodi
-update_musiclibrary
-sleep 1
+update_musiclibrary #update music library on Kodi
 echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - Music sync finished" >> $logfolder/$logname
-################################################################################################################################################################
 #
 #
-#
-#
-################################################################################################################################################################
-# start FILM sync next
+#####################
+## start FILM sync ##
+#####################
 echo "----------------------------------------------------" >> $logfolder/$logname
 echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - Film sync started" >> $logfolder/$logname
 umask $umaskset
 rsync "$rsync_variable1" "$rsync_variable2" "$rsync_switch" "$REMOTE_USER"@"$REMOTE_IP":"$movie_source" "$movie_dest" >> $logfolder/$logname
-# update Video Library on Kodi
-sleep 1
-update_videolibrary
-sleep 1
+update_videolibrary # update Video Library on Kodi
 echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - Movie sync finished" >> $logfolder/$logname
-################################################################################################################################################################
 #
 #
-#
-#
-################################################################################################################################################################
-# start TV downloads sync
+###################
+## start TV sync ##
+###################
 echo "----------------------------------------------------" >> $logfolder/$logname
 echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - TV sync started" >> $logfolder/$logname
 umask $umaskset
 rsync "$rsync_variable1" "$rsync_variable2" "$rsync_switch" "$REMOTE_USER"@"$REMOTE_IP":"$tv_source" "$tv_dest" >> $logfolder/$logname
-# add sleep
-sleep 1
-# update Video Library on Kodi
-update_videolibrary
-sleep 1
+update_videolibrary # update Video Library on Kodi
 echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - TV sync finished" >> $logfolder/$logname
-################################################################################################################################################################
 #
 #
-#
-#
-################################################################################################################################################################
-# start NFL downloads sync
+####################
+## start NFL sync ##
+####################
 echo "----------------------------------------------------" >> $logfolder/$logname
 echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - NFL sync started" >> $logfolder/$logname
 umask $umaskset
 rsync "$rsync_variable1" "$rsync_variable2" "$rsync_switch" "$REMOTE_USER"@"$REMOTE_IP":"$nfl_source" "$nfl_dest" >> $logfolder/$logname
-# add sleep
-sleep 1
-# update Video Library on Kodi
-update_videolibrary
-sleep 1
 echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - NFL sync finished" >> $logfolder/$logname
-################################################################################################################################################################
-#
-#
-# one more time for luck
-update_videolibrary
-update_musiclibrary
-# end
 echo "----------------------------------------------------" >> $logfolder/$logname
 echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - $locknamelong complete" >> $logfolder/$logname
+echo "----------------------------------------------------" >> $logfolder/$logname
 exit 0
