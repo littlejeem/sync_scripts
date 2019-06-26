5a926ac711d3361475a12b102157946dadf030c2
littlejeem <jlivin25@live.co.uk>
2019-06-26 23:02:49 +0100

    quoted variables, picked up n some errors, tweaked description

--- MusicSync.sh
+++ MusicSync.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 #
 #
-#############################################################################################
-## this script is to import music transferred from the raspberry Pi into Beets Librtary   ###
-## Once imported and converted it moves the fies to my music library                      ###
-## script is in $HOME/bin folder                                                          ###
-#############################################################################################
+#################################################################################################
+## This script is to import music transferred from the a remote location into Beets Librtary   ##
+## Once imported and converted it moves the fies to my music library, this is also done with   ##
+## any ripped music                                                                            ##
+## script is in $HOME/scripts/control_scripts folder                                                               ##
+#################################################################################################
 #
 ####################################
 ## Import settings data from file ##
@@ -35,40 +36,40 @@ clean_KodiAudio
 #
 # start MUSIC Import
 # convert flacs to alac and copy to the alac library imports first by using -c flag to specify an alternative config to merge
-$beets_path/beet $beets_switch $beets_alac_path/config.yaml import -q $download_flac
-rm $beets_alac_path/musiclibrary.blb
-$beets_path/beet $beets_switch $beets_alac_path/config.yaml import -q $rip_flac
-rm $beets_alac_path/musiclibrary.blb
+"$beets_path"/beet "$beets_switch" "$beets_alac_path"/config.yaml import -q "$download_flac"
+rm "$beets_alac_path"/musiclibrary.blb
+"$beets_path"/beet "$beets_switch" "$beets_alac_path"/config.yaml import -q "$rip_flac"
+rm "$beets_alac_path"/musiclibrary.blb
 # correct the flac file tags now and move to the flac import library using -c flac to specify an alternative config to merge
-$beets_path/beet $beets_switch $beets_flac_path/config.yaml import -q $download_flac
-rm $beets_flac_path/musiclibrary.blb
-$beets_path/beet $beets_switch $beets_flac_path/config.yaml import -q $rip_flac
-rm $beets_flac_path/musiclibrary.blb
+"$beets_path"/beet "$beets_switch" "$beets_flac_path"/config.yaml import -q "$download_flac"
+rm "$beets_flac_path"/musiclibrary.blb
+"$beets_path"/beet "$beets_switch" "$beets_flac_path"/config.yaml import -q "$rip_flac"
+rm "$beets_flac_path"/musiclibrary.blb
 #
 # sync tagged flac files next
-cd $flaclibrary_source
+cd "$flaclibrary_source"
 DIR=$PWD
 if [ ! "$(ls -A "$DIR")" ]
 then
-    echo "$DIR is empty, no action"
+    echo "$DIR" is empty, no action"
 else
-    echo "$DIR is not empty, copying then deleting files"
+    echo "$DIR" is not empty, copying then deleting files"
     sleep 5s
-    cp -rpv $flaclibrary_source/* $FLAC_musicdest
+    cp -rpv "$flaclibrary_source"/* "$FLAC_musicdest"
     rm -r *
 fi
 #
 #
 # sync alac music files next
-cd $alaclibrary_source
+cd "$alaclibrary_source"
 DIR=$PWD
 if [ ! "$(ls -A "$DIR")" ]
 then
-    echo "$DIR is empty, no action"
+    echo "$DIR" is empty, no action"
 else
-    echo "$DIR is not empty, copying then deleting files"
+    echo "$DIR" is not empty, copying then deleting files"
     sleep 5s
-    cp -rpv $alaclibrary_source/* $M4A_musicdest
+    cp -rpv "$alaclibrary_source"/* "$M4A_musicdest"
     rm -r *
     update_KodiAudio
     sleep 5s
