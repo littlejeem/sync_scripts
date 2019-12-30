92580ecb9e34e0896a5866d5dd9c8e27cb6ef92d
James Livingstone <jlivin25@live.co.uk>
2019-12-30 22:39:47 +0000

    removed assignable variables to a config file. reworked functions

--- AutoBackupInt2Ext.sh
+++ AutoBackupInt2Ext.sh
@@ -9,31 +9,37 @@
 scriptname=`basename "$0"`      # imports the name of this script
 lockname=${scriptname::-3}    # reduces the name to remove .sh
 lockdir=/tmp/$lockname.lock     # name of the lock dir to be used
-sleeptime=30                    # dfines how long should the sleep period be, not necessary once script working, default is seconds, append with 'm' if wanting minutes
-mount="/media/Seagate_Ext"
-uuid="176c6d49-7b6c-46e6-b9be-f0440f5e8141"
-scriptlocation="$HOME/scripts"
-loglocation="$HOME/bin/scriptlogs"
-rsyncsource=/media/Data_1/
-rsyncdestination=/media/Seagate_Ext/Backups/
 #
 #
 ########################
 ## Define "Functions" ##
 ########################
-function Pushover
+function Timestamp ()
+{
+  echo "$(date +%d/%m/%Y) - $(date +%H:%M:%S) - $1"
+}
+#
+function Pushover ()
 {
   curl -s --form-string "token=$(app_token)"   --form-string "user=$(user_token)" --form-string "message=$(message_form)" https://api.pushover.net/1/messages.json
 }
 #
-function rsync_command
+function rsync_command ()
 {
-  echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - script name = "$scriptname
-  echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - lock name will be = "$lockname
-  echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - lockdir will be = "$lockdir
+  Timestamp_message=$(echo "script name = $scriptname")
+  Timestamp "$Timestamp_message"
+  Timestamp_message=$(echo "lock name will be = $lockname")
+  Timestamp "$Timestamp_message"
+  Timestamp_message=$(echo "lock dir will be = $lockdir")
+  Timestamp "$Timestamp_message"
+  Timestamp_message=$(echo "lock name will be = $lockname")
+  Timestamp "$Timestamp_message"
+  Timestamp_message=$(echo "sync started")
+  Timestamp "$Timestamp_message"
   message_form=$(echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - sync started")
   rsync -avrvi --delete --exclude 'lost+found' --progress $rsyncsource $rsyncdestination --log-file="$logdir"/"$stamp".log
-  echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - sync completed"
+  Timestamp_message=$(echo "sync completed")
+  Timestamp "$Timestamp_message"
 }
 #
 #
@@ -64,19 +70,20 @@ if mkdir "$lockdir"
     if command ; then
       echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - sync completed"
       message_form=$(echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - sync completed")
-      rm -r $lockdir          #remove the lockdir once used
-      umount $mountpoint
+      Pushover
+      rm -r "$lockdir"          #remove the lockdir once used
+      umount "$mountpoint"
       echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - Hard Drive $mountpoint unmounted"
       echo "-------------------------------------------------------------------------"
       exit 0 #statements
     else
-
+       exit 1
     fi
     echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - sync completed"
     message_form=$(echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - sync completed")
     Pushover
-    rm -r $lockdir          #remove the lockdir once used
-    umount $mountpoint
+    rm -r "$lockdir"          #remove the lockdir once used
+    umount "$mountpoint"
     echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - Hard Drive  $mountpoint unmounted"
     echo "-------------------------------------------------------------------------"
     exit 0
@@ -88,8 +95,8 @@ if mkdir "$lockdir"
       mount -U "$uuid" "$mount"
       echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - Hard Drive mounted sucessfully"
       rsync_command
-      rm -r $lockdir          #remove the lockdir once used
-      umount $mount
+      rm -r "$lockdir"          #remove the lockdir once used
+      umount "$mount"
       echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - Hard Drive $mount unmounted"
       echo "-------------------------------------------------------------------------"
       exit 0
@@ -103,9 +110,9 @@ if mkdir "$lockdir"
   else
     # echo >&2 "`date +%d/%m/%Y` - `date +%H:%M:%S` - Another instance of this script tried to run, $lockdir"
     echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - Another instance of this script tried to run, $lockdir"
-    rm -r $lockdir
+    rm -r "$lockdir"
     exit 2
     echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - $?"
 fi
 echo "exit $?"
-) >> "$loglocation"/$lockname.log 2>&1 &
+) >> "$loglocation"/"$lockname".log 2>&1 &
