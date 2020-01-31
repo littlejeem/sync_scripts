46eb3c67934c854a29b686f8ec5c06f16f75fa76
James Livingstone <jlivin25@live.co.uk>
2020-01-31 14:28:59 +0000

    added variables to allow chown & chmod with rsync

--- config.sh
+++ config.sh
@@ -10,6 +10,10 @@ user="" #user who will be running the script
 rsync_path="/usr/bin/rsync" #default destination set, change as appropriate
 rsync_variable1="--protect-args" #default destination set, change as appropriate
 rsync_variable2="--remove-source-files" #default destination set, change as appropriate
+rsync_variable3="-p" #tell rsync you explicitly want to set Directory & File Permissions
+rsync_variable4="-og" #tell rsync you explicity want to specify the "user" & "group"
+rsync_variable5="--chmod=Dug+rwx,Fug+rwx,o+rx,o-w" #set the permissions, D=directory, F=file
+rsync_variable6="--chown="$user":group" #set the ownership
 rsync_switch="-vzrc" #set to -vzrc by default, only change if you know what you are doing
 rsync_altswitch="-vzr"
 logfolder="/home/"$user"/bin/scriptlogs"
