9273cd08651ab9b6aada0b08305186298553c2b0
James Livingstone <littlejeem@protonmail.com>
2021-01-27 22:28:06 +0000

    trial using function in helper script for exit code interpreting

--- syncmediadownloads.sh
+++ syncmediadownloads.sh
@@ -122,14 +122,15 @@ then
   rsync_error_catch
   log "Starting MusicSync.sh"
   sudo -u jlivin25 $HOME/bin/sync_scripts/MusicSync.sh #run seperate 'tagger' script
-  reply=$?
-  if [[ "$reply" = 0 ]]; then
-    log "MusicSync.sh exited gracefully"
-  else
-    log_err "Exit code: $reply received"
-    log_deb "MusicSync.sh exited with error"
-  fi
-  log "MUSIC sync finished"
+  script_exit
+#  reply=$?
+#  if [[ "$reply" = 0 ]]; then
+#    log "MusicSync.sh exited gracefully"
+#  else
+#    log_err "Exit code: $reply received"
+#    log_deb "MusicSync.sh exited with error"
+#  fi
+#  log "MUSIC sync finished"
 else
   log "MUSIC sync DESELECTED, no sync"
 fi
