bd815f7f931a95dc47f96f8d53bef26c52e3fe9e
James Livingstone <littlejeem@protonmail.com>
2021-01-31 09:36:39 +0000

    moved to new systemlog logger, repeat functions from helper script

--- AutoBackupInt2Ext.sh
+++ AutoBackupInt2Ext.sh
@@ -3,97 +3,109 @@
 # script is kept in /usr/local/bin and should be made executable with 0755 or 0766 perms
 #
 #
-########################
-## Define "Variables" ##
-########################
+#+------------------+
+#+---"Exit Codes"---+
+#+------------------+
+# pick from 64 - 113 (https://tldp.org/LDP/abs/html/exitcodes.html#FTN.AEN23647)
+# exit 0 = Success
+# exit 64 = Variable Error
+# exit 65 = Sourcing file error
+# exit 66 = Processing Error
+# exit 67 = Required Program Missing
+#
+#
+#+-------------------+
+#+---"Set Version"---+
+#+-------------------+
+version="2.0"
+#
+#
+#+------------------------+
+#+---"Define Variables"---+
+#+------------------------+
 scriptname=`basename "$0"`      # imports the name of this script
 lockname=${scriptname::-3}    # reduces the name to remove .sh
-lockdir=/tmp/$lockname.lock     # name of the lock dir to be used
+lockdir=/tmp/"$lockname"     # name of the lock dir to be used
 #
 #
-########################
-## Define "Functions" ##
-########################
-function Timestamp ()
+#+------------------------+
+#+---"Define Functions"---+
+#+------------------------+
+rsync_command ()
 {
-  echo "$(date +%d/%m/%Y) - $(date +%H:%M:%S) - $1"
-}
-#
-function Pushover ()
-{
-  curl -s --form-string token="$backup_app_token" --form-string user="$user_token" --form-string message="$message_form" https://api.pushover.net/1/messages.json
-}
-#
-function rsync_command ()
-{
-  Timestamp_message=$(echo "script name = $scriptname")
-  Timestamp "$Timestamp_message"
-  Timestamp_message=$(echo "lock name will be = $lockname")
-  Timestamp "$Timestamp_message"
-  Timestamp_message=$(echo "lock dir will be = $lockdir")
-  Timestamp "$Timestamp_message"
-  Timestamp_message=$(echo "sync started")
-  Timestamp "$Timestamp_message"
+  log "script name = $scriptname"
+  log "lock name will be = $lockname"
+  log "lock dir will be = $lockdir"
+  log "sync started"
   message_form=$(echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - NOTICE - sync started")
   Pushover
-  rsync -avrvi --delete --exclude 'lost+found' --progress $rsyncsource $rsyncdestination --log-file="$loglocation"/"$Timestamp".log
+#  rsync -avrvi --delete --exclude 'lost+found' --progress $rsyncsource $rsyncdestination --log-file="$loglocation"/"$timestamp".log
 }
 #
-function exit_segment ()
+exit_segment ()
 {
   rm -r "$lockdir"          #remove the lockdir once used
   umount "$mountpoint"
-  echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - SUCCESS - sync completed"
-  echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - Hard Drive "$mountpoint" unmounted"
+  log "SUCCESS - sync completed"
+  log "Hard Drive $mountpoint unmounted"
   message_form=$(echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - SUCCESS - sync completed, unplug the drive")
   Pushover
-  echo "-------------------------------------------------------------------------"
   exit 0
 }
 #
 #
-####################
-## import scripts ##
-####################
+#+----------------------+
+#+---"Import scripts"---+
+#+----------------------+
 source /home/jlivin25/.config/ScriptSettings/sync_config.sh
+source /home/jlivin25/bin/standalone_scripts/helper_script.sh
 #
 #
-##################
-## start script ##
-##################
-echo "-------------------------------------------------------------------------"
-echo "$(date +%d/%m/%Y) - $(date +%H:%M:%S) - script name = $scriptname"
+#+---------------------+
+#+---"Initial Setup"---+
+#+---------------------+
+#Grab PID
+script_pid=$(echo $$)
+log_deb "Script $scriptname running, PID is: $script_pid"
+#display version
+log_deb "Version is: $version"
+#
+#
+#+--------------------+
+#+---"Start Script"---+
+#+--------------------+
+log "script name = $scriptname"
 if mkdir "$lockdir"
 then   # lock directory did not exist, but was created successfully
  grep -qs "$mount" /proc/mounts #if grep sees the mount then it will return a silent 0 if not seen a silent 1
    if [[ $? -eq 0 ]]
    then
-     echo "$(date +%d/%m/%Y) - $(date +%H:%M:%S) - NOTICE - Hard Drive already mounted"
+     log_deb "Hard Drive already mounted"
      mountpoint=$(grep "$mount" /proc/mounts | cut -c 1-9)
-     echo "mountpoint is $mountpoint"
+     log "mountpoint is $mountpoint"
      rsync_command
      exit_segment
    else
      if [[ $? -eq 1 ]]
      then
-     echo "$(date +%d/%m/%Y) - $(date +%H:%M:%S) - NOTICE - Hard Drive NOT currently mounted."
+     log_deb "Hard Drive NOT currently mounted."
        if mount -U "$uuid" "$mount"
        then
-        echo "$(date +%d/%m/%Y) - $(date +%H:%M:%S) - NOTICE - Hard Drive mounted successfully"
+        log "Hard Drive mounted successfully"
         rsync_command
         exit_segment
        else
-        echo "$(date +%d/%m/%Y) - $(date +%H:%M:%S) - ERROR - Something went wrong with the mount..."
+        log_err "Something went wrong with the mount..."
         message_form=$(echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - ERROR - Something went wrong with the mount...")
         Pushover
-        echo "-------------------------------------------------------------------------"
-        exit 1
+        exit 66
        fi
      fi
    fi
 else
-  echo "$(date +%d/%m/%Y) - $(date +%H:%M:%S) - ERROR - Another instance of this script tried to run, $lockdir"
+  log_err "Another instance of this script tried to run, $lockdir"
   message_form=$(echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - ERROR - Another instance of this script tried to run...")
   Pushover
-  exit 1
+  exit 66
 fi
+exit 0
