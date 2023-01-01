7c04f0403449ae7f6de4a452f135ba2b135baf26
James Livingstone <littlejeem@protonmail.com>
2023-01-01 19:59:26 +0000

    restructured, few fixes

--- AutoBackupInt2Ext.sh
+++ AutoBackupInt2Ext.sh
@@ -12,7 +12,7 @@
 # exit 65 = Sourcing file/folder error
 # exit 66 = Processing Error
 # exit 67 = Required Program Missing
-#
+
 #verbosity levels
 #silent_lvl=0
 #crt_lvl=1
@@ -21,14 +21,14 @@
 #ntf_lvl=4
 #inf_lvl=5
 #dbg_lvl=6
-#
-#
+
+
 #+-------------------+
 #+---"Set Version"---+
 #+-------------------+
-version="2.0"
-#
-#
+version="3.0"
+
+
 #+------------------------+
 #+---"Define Variables"---+
 #+------------------------+
@@ -37,10 +37,28 @@ lockname=${scriptname::-3}    # reduces the name to remove .sh
 lockdir=/tmp/"$lockname"     # name of the lock dir to be used
 #set default logging level
 verbosity=3
-#
-#
+
+
+#+----------------------+
+#+---"Import scripts"---+
+#+----------------------+
+source /usr/local/bin/config.sh
+source /usr/local/bin/helper_script.sh
+
+
+#+---------------------+
+#+---"Initial Setup"---+
+#+---------------------+
+#Grab PID
+esilent "$lockname script started"
+script_pid=$(echo $$)
+edebug "Script $scriptname running, PID is: $script_pid"
+#display version
+edebug "Version is: $version"
+
+
 #+------------------------+
-#+---"Define Functions"---+
+#+--- Define Functions ---+
 #+------------------------+
 rsync_command ()
 {
@@ -51,12 +69,13 @@ rsync_command ()
   message_form=$(echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - NOTICE - sync started")
   pushover > /dev/null
   if [[ $dry_run -eq 1 ]]; then
-    edebug "dry-run enabled, rsync normally would run but disabled"
+    edebug "dry-run enabled, calling rsync with dry-run flag"
+    rsync --dry-run -avrvi --delete --exclude 'lost+found' --progress $rsyncsource $rsyncdestination --log-file="$loglocation"/"$timestamp".log
   else
     rsync -avrvi --delete --exclude 'lost+found' --progress $rsyncsource $rsyncdestination --log-file="$loglocation"/"$timestamp".log
   fi
 }
-#
+
 exit_segment ()
 {
   umount /dev/disk/by-uuid/"$uuid"
@@ -65,42 +84,32 @@ exit_segment ()
   message_form=$(echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - SUCCESS - sync completed, unplug the drive")
   pushover > /dev/null
   rm -r "$lockdir"          #remove the lockdir once used
+  clean_exit
+}
+
+clean_ctrlc () {
+  let ctrlc_count++
+  echo
+  if [[ $ctrlc_count == 1 ]]; then
+    echo "Quit command detected, are you sure?"
+  elif [[ $ctrlc_count == 2 ]]; then
+    echo "...once more and the script will exit..."
+  else
+    clean_exit
+  fi
+}
+
+clean_exit () {
+  edebug "Exiting script gracefully"
+  rm -r /tmp/"$lockname"
   exit 0
 }
-#
-#
-#+----------------------+
-#+---"Import scripts"---+
-#+----------------------+
-source /usr/local/bin/config.sh
-source /usr/local/bin/helper_script.sh
-#
-#
-#+---------------------+
-#+---"Initial Setup"---+
-#+---------------------+
-#Grab PID
-esilent "$lockname script started"
-script_pid=$(echo $$)
-edebug "Script $scriptname running, PID is: $script_pid"
-#display version
-edebug "Version is: $version"
-#
-#
-#+---------------------------------------+
-#+---"Check if script running already"---+
-#+---------------------------------------+
-check_running
-#
-#
-#+------------------------+
-#+--- Define Functions ---+
-#+------------------------+
+
 helpFunction () {
    echo ""
    echo "Usage: $0"
    echo "Usage: $0 -dV selects dry-run with verbose level logging"
-   echo -e "\t-d Use this flag to specify dry run, no files will be converted, useful in conjunction with -V or -G "
+   echo -e "\t-d Use this flag to specify dry run, no files will be transferred, useful in conjunction with -V or -G "
    echo -e "\t-s Override set verbosity to specify silent log level"
    echo -e "\t-V Override set verbosity to specify verbose log level"
    echo -e "\t-G Override set verbosity to specify Debug log level"
@@ -112,7 +121,8 @@ helpFunction () {
    fi
    exit 1 # Exit script after printing help
 }
-#
+
+
 #+------------------------+
 #+---"Get User Options"---+
 #+------------------------+
@@ -133,24 +143,40 @@ do
     esac
 done
 shift $((OPTIND -1))
-#
-#
+
+
+#+---------------------+
+#+---"Trap & ctrl-c"---+
+#+---------------------+
+trap clean_ctrlc SIGINT
+trap clean_exit SIGTERM
+ctrlc_count=0
+
+
+#+---------------------------------------+
+#+---"Check if script running already"---+
+#+---------------------------------------+
+check_running
+
+
 #+--------------------------------------------+
 #+---Check that necessary variables are set---+
 #+--------------------------------------------+
+edebug "Checking necessary variables are set..."
 JAIL_FATAL="${scriptname}"
 fatal_missing_var
-#
+
 JAIL_FATAL="${lockdir}"
 fatal_missing_var
-#
+
 JAIL_FATAL="${rsyncsource}"
 fatal_missing_var
-#
+
 JAIL_FATAL="${rsyncdestination}"
 fatal_missing_var
-#
-#
+edebug "...necessary variables are set"
+
+
 #+--------------------+
 #+---"Start Script"---+
 #+--------------------+
@@ -158,7 +184,7 @@ grep -qs "$mount" /proc/mounts #if grep sees the mount then it will return a sil
 if [[ $? -eq 0 ]]; then
   edebug "Hard Drive already mounted"
   mountpoint=$(grep "$mount" /proc/mounts | cut -d ' ' -f 1)
-  enotify "mountpoint is $mountpoint"
+  edebug "mountpoint is $mountpoint"
   rsync_command
   exit_segment
 else
@@ -169,7 +195,7 @@ else
       rsync_command
       exit_segment
     else
-      log_err "Something went wrong with the mount..."
+      eerror "Something went wrong with the mount..."
       message_form=$(echo "`date +%d/%m/%Y` - `date +%H:%M:%S` - ERROR - Something went wrong with the mount...")
       pushover > /dev/null
       exit 66
@@ -177,4 +203,4 @@ else
   fi
 fi
 esilent "$lockname script finished"
-exit 0
+clean_exit
